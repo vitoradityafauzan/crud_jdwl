db1c76944b8927ae2fc16d7d065fd52bf4874f23
Vito Raditya Fauzan <vitoraditya.f@gmail.com>
2018-03-12 12:54:16 +0700

    mk-1.1
    .

--- pdo_ajax_crud_datatables/index.php
+++ pdo_ajax_crud_datatables/index.php
@@ -1,10 +1,10 @@
 <html>
 	<head>
 		<title>Webslesson Demo - PHP PDO Ajax CRUD with Data Tables and Bootstrap Modals</title>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-		<script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
-		<script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>		
+		<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+		<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>		
 		<link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 		<style>
@@ -69,8 +69,7 @@
 					<br />
 					<label>Enter Last Name</label>
 					<input type="text" name="last_name" id="last_name" class="form-control" />
-					<br />
-	
+					<br />	
 				</div>
 				<div class="modal-footer">
 					<input type="hidden" name="id_job" id="id_job" />
